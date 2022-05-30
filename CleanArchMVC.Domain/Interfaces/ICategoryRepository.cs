514c943b8e737c43ced941a88a7dcd54276922ad
JLAChaves <lucas2015nxi@gmail.com>
2022-05-30 17:32:02 -0400

    Interfaces created in the domain

---
+++ CleanArchMVC.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMVC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategori();
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMVC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProductCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}

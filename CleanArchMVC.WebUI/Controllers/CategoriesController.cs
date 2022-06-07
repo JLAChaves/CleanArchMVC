e5b3826580e35de93c879df0b5beb5e3ecb296ba
JLAChaves <lucas2015nxi@gmail.com>
2022-06-07 16:31:27 -0400

    Added delete, details and edit categories functionality

--- CleanArchMVC.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMVC.WebUI/Controllers/CategoriesController.cs
@@ -35,5 +35,61 @@ namespace CleanArchMVC.WebUI.Controllers
             }
             return View(category);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+            return View(categoryDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var category = await _categoryService.GetById(id);
+
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var category = await _categoryService.GetById(id);
+
+            if (category == null) return NotFound();
+            return View(category);
+        }   
+
     }
 }
